ecb02deb6d71bb3c581eddd493d470b98a6f56e8
Max <maxweissman@rogers.com>
2025-02-03 15:35:50 -0800

    Remove blue outline from cracker image

--- client/game.js
+++ client/game.js
@@ -2,7 +2,7 @@ import { Game, AUTO } from "phaser";
 import WorldScene from './Worldscene'
 import BattleScene from "./Battlescene";
 
-const player = {name: 'cracker', image: 'assets/Cracker.jfif', type: 'Player', scale: 0.02, hp: 100, damage: 20}
+const player = {name: 'cracker', image: 'assets/Cracker.png', type: 'Player', scale: 0.02, hp: 100, damage: 20}
 const enemy = {name: 'cracker', type: 'Enemy', scale: 0.02,  hp: 100, damage: 20}
 
 const world = new WorldScene([player, enemy])
